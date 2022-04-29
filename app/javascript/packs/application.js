e5f40e547d4f0b5ee1cad9ec3c5353e3f57c52a0
hannangujar <hannan.gujar@gmail.com>
2022-04-29 13:37:53 +0500

    bootstarp

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,10 @@
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+import "bootstrap"
+import "../stylesheets/application"
+
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+require ("stylesheets/application.scss")
